452690c5d263be338ef2acbb947c2923dd3d5c74
bahadiremre <bahadiremre2744@gmail.com>
2020-12-04 21:59:58 +0300

    bring all countries

---
+++ BES.Slot.WEBUI/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using BES.Slot.WEBUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BES.Slot.WEBUI.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private readonly string CountriesURL="https://restcountries.eu/rest/v2/all";
+        public async Task<IActionResult> IndexAsync()
+        {
+            using var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync(CountriesURL);
+            var jsonCountries = await responseMessage.Content.ReadAsStringAsync();
+
+            var countries = JsonConvert.DeserializeObject<List<CountryWithLanguage>>(jsonCountries);
+
+            return View(countries);
+        }
+    }
+}
