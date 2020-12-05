98f6ac66ad2212d01cf6724ee21a963bbff8d55d
bahadiremre <bahadiremre2744@gmail.com>
2020-12-05 18:01:28 +0300

    searching countries on API

--- BES.Slot.WEBUI/Controllers/CountriesController.cs
+++ BES.Slot.WEBUI/Controllers/CountriesController.cs
@@ -12,26 +12,60 @@ namespace BES.Slot.WEBUI.Controllers
 {
     public class CountriesController : Controller
     {
-        private readonly string CountriesURL = "https://restcountries.eu/rest/v2/all";
+        private readonly string CountriesURL = "https://restcountries.eu/rest/v2/all?fields=name;nativeName;flag;languages";
+        private readonly string CountriesSearchingURL = "https://restcountries.eu/rest/v2/name/{name}?fields=name;nativeName;flag;languages?fields=name";
         private IMemoryCache _cache;
         public CountriesController(IMemoryCache memoryCache)
         {
             _cache = memoryCache;
         }
-        public async Task<IActionResult> IndexAsync(int curPage = 0)
+
+        public async Task<IActionResult> IndexAsync(int curPage = 0, string search = "")
         {
-            string jsonCountries = await GetJsonCountriesFromCacheAsync();
+            List<CountryWithLanguage> currentCountries;
+            if (string.IsNullOrEmpty(search))
+            {
+                string jsonCountries = await GetJsonCountriesFromCacheAsync();
 
-            List<CountryWithLanguage> countries = JsonConvert.DeserializeObject<List<CountryWithLanguage>>(jsonCountries);
+                List<CountryWithLanguage> countries = JsonConvert.DeserializeObject<List<CountryWithLanguage>>(jsonCountries);
 
-            int totalPages = (int)Math.Ceiling((double)countries.Count / 8);
-            ViewBag.TotalPages = totalPages;
+                int totalPages = (int)Math.Ceiling((double)countries.Count / 8);
+                ViewBag.TotalPages = totalPages;
 
-            var currentCountries = countries.Skip(curPage * 8).Take(8).ToList();
+                currentCountries = countries.Skip(curPage * 8).Take(8).ToList();
 
-            ViewBag.CurrentPage = curPage;
+                ViewBag.CurrentPage = curPage;
+            }
+            else
+            {
+                ViewBag.CurrentPage = 0;
+                ViewBag.TotalPages = 1;
+                string jsonSCountries = await GetSearchingCountriesFromAPIAsync(search);
+                if (!string.IsNullOrEmpty(jsonSCountries))
+                {
+                    currentCountries = JsonConvert.DeserializeObject<List<CountryWithLanguage>>(jsonSCountries);
+                }
+                else
+                {
+                    currentCountries = new List<CountryWithLanguage>();
+                }
+
+            }
 
             return View(currentCountries);
+        }
+
+        private async Task<string> GetSearchingCountriesFromAPIAsync(string search)
+        {
+            using var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync(CountriesSearchingURL.Replace("{name}", search.ToUpper()));
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return await responseMessage.Content.ReadAsStringAsync();
+            }
+            else
+                return "";
+
         }
 
         private async Task<string> GetJsonCountriesFromCacheAsync()
@@ -45,7 +79,6 @@ namespace BES.Slot.WEBUI.Controllers
 
             return cacheCountries;
         }
-
 
         private async Task<string> GetCountriesAsJsonAsync()
         {
